314440b8badedfdd69e3b47e2beabf1d7e8347ee
Yushan Viduranga <88585505+vidurangaushan@users.noreply.github.com>
2023-04-19 00:16:29 +0530

    First commit

---
+++ yumyum/src/main/java/com/yumyum/paf/YumyumApplication.java
@@ -0,0 +1,13 @@
+package com.yumyum.paf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YumyumApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YumyumApplication.class, args);
+	}
+
+}
---
+++ yumyum/src/test/java/com/yumyum/paf/YumyumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yumyum.paf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YumyumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
