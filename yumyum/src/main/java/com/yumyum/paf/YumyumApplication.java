11a9e9d0da35d5b2884c137de22face3a32def47
Yushan Viduranga <88585505+vidurangaushan@users.noreply.github.com>
2023-04-19 00:52:54 +0530

    Update Controller class

--- yumyum/src/main/java/com/yumyum/paf/YumyumApplication.java
+++ yumyum/src/main/java/com/yumyum/paf/YumyumApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class YumyumApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(YumyumApplication.class, args);
 	}
 
